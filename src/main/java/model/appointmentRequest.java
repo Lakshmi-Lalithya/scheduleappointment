4aaee6819ab4e81d841d292686d7e803f9c01f84
Sahithya <sahithyaaatipamula00@gmail.com>
2022-03-05 13:08:33 +0530

    secondchange

--- src/main/java/model/appointmentRequest.java
+++ src/main/java/model/appointmentRequest.java
@@ -1,5 +1,49 @@
 package model;
 
 public class appointmentRequest {
-
+	int requestNo;
+	String bookingTime;
+	int duration;
+	String bookingStatus;
+	String scheduler;
+	int schedulerId;
+	public int getRequestNo() {
+		return requestNo;
+	}
+	public void setRequestNo(int requestNo) {
+		this.requestNo = requestNo;
+	}
+	public String getBookingTime() {
+		return bookingTime;
+	}
+	public void setBookingTime(String bookingTime) {
+		this.bookingTime = bookingTime;
+	}
+	public int getDuration() {
+		return duration;
+	}
+	public void setDuration(int duration) {
+		this.duration = duration;
+	}
+	public String getBookingStatus() {
+		return bookingStatus;
+	}
+	public void setBookingStatus(String bookingStatus) {
+		this.bookingStatus = bookingStatus;
+	}
+	public int getSchedulerId() {
+		return schedulerId;
+	}
+	public void setSchedulerId(int schedulerId) {
+		this.schedulerId = schedulerId;
+	}
+	public String getScheduler() {
+		return scheduler;
+	}
+	public void setScheduler(String scheduler) {
+		this.scheduler = scheduler;
+	}
+	
+	
+	
 }
